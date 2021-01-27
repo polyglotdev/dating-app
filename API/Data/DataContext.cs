58b5531e06bfc7d393665af1e0f3e5c78b45763c
Dom Hallan <dom@polyglotdev.com>
2021-01-26 19:26:47 -0600

    🔧 complete module 11 in section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,8 @@
+namespace API.Data
+{
+  public class DataContext : DbContext
+  {
+
+    return System.NotImplementedException;
+  }
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -2,5 +2,6 @@ namespace API.Entities {
   public class AppUser
   {
       public int Id { get; set; }
+      public string UserName { get; set; }
   }
 }
