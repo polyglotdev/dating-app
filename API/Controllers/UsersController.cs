b42b999d745d3047ed2a38864b5aa2136e1d3716
Dom Hallan <dom@polyglotdev.com>
2021-01-26 21:26:12 -0600

    🔧 complete section 2

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,6 +3,10 @@ using System.Linq;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace API.Controllers
 {
@@ -19,15 +23,15 @@ namespace API.Controllers
       }
 
       [HttpGet]
-      public ActionResult<IEnumerable<AppUser>> GetUsers()
+      public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
       {
-        return  _context.Users.ToList();
+        return  await _context.Users.ToListAsync();
       }
       
       [HttpGet("{id}")]
-      public ActionResult<AppUser> GetUser(int id)
+      public async Task<ActionResult<AppUser>> GetUser(int id)
       {
-        return _context.Users.Find(id);
+        return await _context.Users.FindAsync(id);
       }
     }
 }
