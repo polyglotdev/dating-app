8b9ed2763d61bfc67822189018e57768767ce419
Dom Hallan <dom@polyglotdev.com>
2021-02-18 17:04:12 -0600

    🔧 add protection to endpoints for GetUsers and GetUsers(\"{id})\"

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
@@ -21,11 +21,13 @@ namespace API.Controllers
       }
 
       [HttpGet]
+      [AllowAnonymous]
       public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
       {
         return  await _context.Users.ToListAsync();
       }
-      
+
+      [Authorize]
       [HttpGet("{id}")]
       public async Task<ActionResult<AppUser>> GetUser(int id)
       {
