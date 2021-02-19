2bc632b3d312f5abadba08c06dfd92730b0b4b9a
Dom Hallan <dom@polyglotdev.com>
2021-02-18 18:03:47 -0600

    🔧 add navbar component to the client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { HttpClientModule } from '@angular/common/http'
 
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
