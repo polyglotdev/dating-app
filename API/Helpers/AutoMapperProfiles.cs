c6f5403c497873c8ec83d6daaf61beae51ef7912
Dom Hallan <dom@polyglotdev.com>
2021-03-11 13:20:20 -0600

    🔧  add mapping to get photo url

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System;
 using API.DTOs;
 using API.Entities;
@@ -9,8 +10,10 @@ namespace API.Helpers
   {
     public AutoMapperProfiles()
     {
-      CreateMap<AppUser, MemberDto>();
+      CreateMap<AppUser, MemberDto>().ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src
+        => src.Photos.FirstOrDefault(x => x.IsMain).Url));
       CreateMap<Photo, PhotoDto>();
+
     }
   }
 }
