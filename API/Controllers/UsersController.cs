19ab883cab5e592f9bb35f55bbba3f98a6323ba2
Dom Hallan <dom@polyglotdev.com>
2021-03-11 11:24:53 -0600

    🎆 add authorize decorator on users controller

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -10,28 +10,27 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
+  [Authorize]
   public class UsersController : BaseApiController
+  {
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
     {
-      private readonly DataContext _context;
+      _context = context;
 
-      public UsersController(DataContext context)
-      {
-        _context = context;
-        
-      }
-
-      [HttpGet]
-      [AllowAnonymous]
-      public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
-      {
-        return  await _context.Users.ToListAsync();
-      }
-
-      [Authorize]
-      [HttpGet("{id}")]
-      public async Task<ActionResult<AppUser>> GetUser(int id)
-      {
-        return await _context.Users.FindAsync(id);
-      }
     }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+      return await _context.Users.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+      return await _context.Users.FindAsync(id);
+    }
+  }
 }
