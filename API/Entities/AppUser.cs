c6f01b56b04766e093df48b4737df9b152095448
Dom Hallan <dom@polyglotdev.com>
2021-03-11 13:11:58 -0600

    🔧  add photo url prop to photo dto

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -6,21 +6,21 @@ namespace API.Entities
 {
   public class AppUser
   {
-    public int Id { get; set; }
-    public string UserName { get; set; }
-    public byte[] PasswordHash { get; set; }
-    public byte[] PasswordSalt { get; set; }
-    public string Gender { get; set; }
-    public string Introduction { get; set; }
-    public string LookingFor { get; set; }
-    public string KnownAs { get; set; }
-    public string Interests { get; set; }
     public string City { get; set; }
     public string Country { get; set; }
-    public ICollection<Photo> Photos { get; set; }
-    public DateTime DateOfBirth { get; set; } = DateTime.Now;
     public DateTime Created { get; set; } = DateTime.Now;
+    public DateTime DateOfBirth { get; set; } = DateTime.Now;
+    public string Gender { get; set; }
+    public int Id { get; set; }
+    public string Interests { get; set; }
+    public string Introduction { get; set; }
+    public string KnownAs { get; set; }
     public DateTime LastActive { get; set; } = DateTime.Now;
+    public string LookingFor { get; set; }
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
+    public ICollection<Photo> Photos { get; set; }
+    public string UserName { get; set; }
 
     public int GetAge()
     {
