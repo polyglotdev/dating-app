0ddaf2132322a0711fdc349622ade5e95b1d5d70
Dom Hallan <dom@polyglotdev.com>
2021-04-23 17:09:10 -0500

    🎆  add getMember method to member service

--- client/src/app/services/members.service.ts
+++ client/src/app/services/members.service.ts
@@ -2,7 +2,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { environment } from '../../environments/environment'
 import { Member } from '../models/member'
-import { Observable } from 'rxjs'
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -17,7 +16,14 @@ export class MembersService {
   baseUrl = environment.apiUrl
   constructor(private http: HttpClient) {}
 
-  getMembers(): Observable<Member[]> {
+  getMembers() {
     return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions)
+  }
+
+  getMember(username: string) {
+    return this.http.get<Member>(
+      this.baseUrl + 'users/' + username,
+      httpOptions
+    )
   }
 }
