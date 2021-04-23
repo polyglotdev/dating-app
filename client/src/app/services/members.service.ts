091f6b88ad3eb3f6150c2844f8fbb8dd7e58215f
Dom Hallan <dom@polyglotdev.com>
2021-04-23 17:05:38 -0500

    🎆  add logic to member service

--- client/src/app/services/members.service.ts
+++ client/src/app/services/members.service.ts
@@ -1,10 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { environment } from '../../environments/environment'
+import { Member } from '../models/member'
+import { Observable } from 'rxjs'
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: 'Bearer' + JSON.parse(localStorage.getItem('user')).token
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
   baseUrl = environment.apiUrl
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  getMembers(): Observable<Member[]> {
+    return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions)
+  }
 }
